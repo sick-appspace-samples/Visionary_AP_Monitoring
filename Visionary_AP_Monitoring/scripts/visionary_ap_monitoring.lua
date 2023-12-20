da6a9ccc1fd1fdd2ac087e2223469a7cb38e8bc5
SICKAppSpace-dev <>
2023-12-20 23:59:01 +0000

    Update

--- Visionary_AP_Monitoring/scripts/visionary_ap_monitoring.lua
+++ Visionary_AP_Monitoring/scripts/visionary_ap_monitoring.lua
@@ -1,19 +1,19 @@
 --[[----------------------------------------------------------------------------
 
   Application Name: Visionary_AP_ Monitoring
-  
+
   Summary:
   Show interface and main board temperature.
-  
+
   Description:
   Use the Monitor to get the temperature.
-  
+
   How to run:
   Start by running the app (F5) or debugging (F7+F10).
   Set a breakpoint on the first row inside the main function to debug step-by-step.
   See the results in the Console.
-  
-  
+
+
 ------------------------------------------------------------------------------]]
 
 --Start of Global Scope---------------------------------------------------------
--- readme.md
+++ readme.md
@@ -1,12 +1,17 @@
 ## Visionary_AP_Monitoring
+
 Show interface and main board temperature.
 
 ### Description
+
 Use the Monitor to get the interface and main board temperature.
+
 ### How to run
-Start by running the app (F5) or debugging (F7+F10). 
-Set a breakpoint on the first row inside the main function to debug step-by-step. 
+
+Start by running the app (F5) or debugging (F7+F10).
+Set a breakpoint on the first row inside the main function to debug step-by-step.
 See the results in the viewer on the DevicePage.
 
 ### Topics
-Monitoring, Visionary, Sample, SICK-AppSpace
+
+monitoring, visionary, sample, sick-appspace
